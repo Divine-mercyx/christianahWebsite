a900849f68854ccff1e4bcb322c068dda3a88e65
christianahOlude <oludechristianah16@gmail.com>
2025-07-28 14:45:57 +0100

    Worked on the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {Navbar} from './components/navbar.jsx'
 import {HeroSection} from "./components/heroSection.jsx";
+
 export function App() {
 
   return (
       <>
-      <Navbar/>
-      <HeroSection/>
+          <Navbar/>
+          <HeroSection/>
+
       </>
   )
 }
